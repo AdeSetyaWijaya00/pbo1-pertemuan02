6e888babf5c990ee0f41a2da550e8cea6aa7d34a
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-03-25 10:17:12 +0800

    Memanggil method dari Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ==============");
+  System.out.println("NIDN : 1103058000");
+  System.out.println("Nama : Ade Setya Wijaya");
+  System.out.println("============================");
+ }
+}
